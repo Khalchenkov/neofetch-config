ad8c610691ac454bca96896bb0972bd51811cb25
Khalchenkov <114304543+Khalchenkov@users.noreply.github.com>
2023-09-29 17:45:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 md ~/.config/neofetch
 md ~/.config/neofetch/normal ~/.config/neofetch/small
-cp neofetch-config/normal.conf ~/.config/neofetch/normal/config.conf
-cp neofetch-config/normal_logo ~/.config/neofetch/normal/logo
-cp neofetch-config/normal_logo_agro ~/.config/neofetch/normal/logo_agro
-cp neofetch-config/small.conf ~/.config/neofetch/small/config.conf
-cp neofetch-config/small_logo ~/.config/neofetch/small/logo
+cp normal.conf ~/.config/neofetch/normal/config.conf
+cp normal_logo ~/.config/neofetch/normal/logo
+cp normal_logo_agro ~/.config/neofetch/normal/logo_agro
+cp small.conf ~/.config/neofetch/small/config.conf
+cp small_logo ~/.config/neofetch/small/logo
+cd ..
 rd neofetch-config
