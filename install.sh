a42b35286c5b70893cad49bf263ab021999069ca
Khalchenkov <114304543+Khalchenkov@users.noreply.github.com>
2023-09-29 18:01:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
+# Remove old neofetch config
+rm -rf ~/.config/neofetch
+
+# Create directory
 mkdir ~/.config/neofetch
 mkdir ~/.config/neofetch/normal ~/.config/neofetch/small
+
+# Copy config and image
 cp normal.conf ~/.config/neofetch/normal/config.conf
 cp normal_logo ~/.config/neofetch/normal/logo
 cp normal_logo_agro ~/.config/neofetch/normal/logo_agro
 cp small.conf ~/.config/neofetch/small/config.conf
 cp small_logo ~/.config/neofetch/small/logo
+
+# Remove source directory
 cd ..
 rm -rf neofetch-config
